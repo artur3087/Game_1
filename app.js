f549c246d5d7edfa0e3cd2173cc73a4651255e72
artur3087 <artur3087@gmail.com>
2024-11-12 20:05:18 +0000

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log("Hello world!") // Testing ;) //
 
+const card1 = document.querySelector("#card1");
+
+card1.addEventListener("click", () => {
+    card1.classList.toggle("flipCard");
+})
+
+const card2 = document.querySelector("#card2");
+
+card2.addEventListener("click", () => {
+    card2.classList.toggle("flipCard");
+})
