3908131a0fb5821530e85cd30653dfaa85d5cc55
artur3087 <artur3087@gmail.com>
2024-11-12 20:13:32 +0000

    third commit

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ const card2 = document.querySelector("#card2");
 card2.addEventListener("click", () => {
     card2.classList.toggle("flipCard");
 })
+
+const card3 = document.querySelector("#card3");
+
+card3.addEventListener("click", () => {
+    card3.classList.toggle("flipCard");
+})
+
